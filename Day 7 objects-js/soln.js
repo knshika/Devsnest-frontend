bcd32cfe085b7126e7d6362a900920510d474f16
knshika <poojakumari17032001@gmail.com>
2021-06-14 18:50:45 +0530

    done with html and css of calculator

--- Day 7 objects-js/soln.js
+++ Day 7 objects-js/soln.js
@@ -1,32 +1,36 @@
 // question 1
 
-// function display(student) {
-//   let arr = Object.keys(student);
-//   return arr.join(",");
+// for(let items in student){
+//   console.log (items);
 // }
 
-// console.log(
-//   display((student = { name: "David Rayy", sclass: "VI", rollno: 12 }))
-// );
+//keys,values,freezegetOwnPropertyName
+
+function display(student) {
+  let arr = Object.keys(student);
+  return arr.join(",");
+}
+
+console.log(display({ name: "David Rayy", sclass: "VI", rollno: 12 }));
 
 // question 2
 
-// function del(student) {
-//   console.log(student);
-//   delete student.rollno;
-//   return student;
-// }
+function del(student) {
+  console.log(student);
+  delete student.rollno;
+  return student;
+}
 
-// console.log(del((student = { name: "David Rayy", sclass: "VI", rollno: 12 })));
+console.log(del({ name: "David Rayy", sclass: "VI", rollno: 12 }));
 
 // question 3
 
-// function len(student) {
-//   let size = Object.keys(student).length;
-//   return size;
-// }
+function len(student) {
+  let size = Object.keys(student).length;
+  return size;
+}
 
-// console.log(len((student = { name: "David Rayy", sclass: "VI", rollno: 12 })));
+console.log(len({ name: "David Rayy", sclass: "VI", rollno: 12 }));
 
 // question 4
 
@@ -40,4 +44,59 @@ var library = [
   },
 ];
 
-console.log(library.readingStatus);
+function displayStatus() {
+  library.forEach((value) => {
+    if (value.readingStatus === true) {
+      console.log("You have read " + value.title);
+    } else {
+      console.log("You haven't read " + value.title);
+    }
+  });
+}
+displayStatus();
+
+/**
+ * Expected output:
+ * You have read The Road Ahead
+ * You have read Walter Issacson
+ * You haven't read Mockingjay: The Final Book of The Hunger Games
+ */
+
+// question 5
+
+class Cylinder {
+  constructor(height, radius) {
+    this.height = height;
+    this.radius = radius;
+  }
+  // Getter
+  get volume() {
+    return this.calcVol();
+  }
+  // Method
+  calcVol() {
+    return this.height * this.radius * 2 * 3.1415;
+  }
+}
+
+const cylinder = new Cylinder(10, 4);
+
+console.log(cylinder.volume.toFixed(4));
+
+// question 6
+
+var library2 = [
+  { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
+  { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    libraryID: 3245,
+  },
+];
+
+library2.sort(function (a, b) {
+  return b.libraryID - a.libraryID;
+});
+
+console.log(library2);
